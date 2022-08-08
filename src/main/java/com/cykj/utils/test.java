e40bdc5f81edf18fbc4063278ae7ffa3adf0d2c0
“Eweark” <727954956@qq.com>
2022-08-08 14:30:46 +0800

    0808测试2

--- src/main/java/com/cykj/utils/test.java
+++ src/main/java/com/cykj/utils/test.java
@@ -3,5 +3,7 @@ package com.cykj.utils;
 public class test {
     public static void main(String[] args) {
         System.out.println("第一个");
+        System.out.println("die ");
+        System.out.println("第三个");
     }
 }
